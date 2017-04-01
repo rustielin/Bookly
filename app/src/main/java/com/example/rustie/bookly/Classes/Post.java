83b79384773acd1a7a99e2fe84aff3554f29f362
rustielin <rustielin@berkeley.edu>
2017-04-01 14:10:26 -0700

    post adapters and stuff

--- app/src/main/java/com/example/rustie/bookly/Classes/Post.java
+++ app/src/main/java/com/example/rustie/bookly/Classes/Post.java
@@ -16,4 +16,64 @@ public class Post {
     private String dept;
     private int class_num;
     private String book_gs;
+
+    public static String getGs_bucket() {
+        return gs_bucket;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public int getClass_num() {
+        return class_num;
+    }
+
+    public void setClass_num(int class_num) {
+        this.class_num = class_num;
+    }
+
+    public String getBook_gs() {
+        return book_gs;
+    }
+
+    public void setBook_gs(String book_gs) {
+        this.book_gs = book_gs;
+    }
 }
