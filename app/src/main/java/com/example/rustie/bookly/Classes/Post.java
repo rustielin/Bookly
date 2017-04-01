46aef3f8aef2317a2a4d6b3dbb9218daef0c661c
Nadir Akhtar <nadirakhtar123@gmail.com>
2017-04-01 13:44:53 -0700

    Initializing Post.java

---
+++ app/src/main/java/com/example/rustie/bookly/Classes/Post.java
@@ -0,0 +1,19 @@
+package com.example.rustie.bookly.Classes;
+
+
+/**
+ * Created by nader on 4/1/17.
+ */
+
+public class Post {
+    
+    public static final String gs_bucket = "gs://bookly-77709.appspot.com/";
+
+    private int ISBN;
+    private String quality;
+    private int price;
+    private String description;
+    private String dept;
+    private int class_num;
+    private String book_gs;
+}
